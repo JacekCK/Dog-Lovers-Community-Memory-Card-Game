fb3808a17de5b177dfbbbe333910d6b8e553f36c
JacekCK <jacek.f.szwaczka@gmail.com>
2022-12-20 14:54:27 +0000

    add username before page loaded

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// must write name before page loaded
+
+let username;
+
+function requiredInput() {
+    do {
+        username = prompt("Welcome to Dog Lovers Memory Game \n\nTo proceed please enter your Name: ");
+    }
+    while (username.length < 2);
+    document.getElementById("myinput").value = username;
+
+};
+requiredInput();
+
