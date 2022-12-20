8cff0d66886f8f8c379a4db8c6cfad4414a4e251
JacekCK <jacek.f.szwaczka@gmail.com>
2022-12-20 15:02:16 +0000

    Add DOM content loader and generating card image data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,54 @@
-// must write name before page loaded
+document.addEventListener('DOMContentLoaded', () => {
 
-let username;
-
-function requiredInput() {
-    do {
-        username = prompt("Welcome to Dog Lovers Memory Game \n\nTo proceed please enter your Name: ");
-    }
-    while (username.length < 2);
-    document.getElementById("myinput").value = username;
-
-};
-requiredInput();
-
+    // card options
+    const cardArray = [
+        {
+            name: 'dog1',
+            img: 'assets/images/dog1.png'
+        },
+        {
+            name: 'dog1',
+            img: 'assets/images/dog1.png'
+        },
+        {
+            name: 'dog2',
+            img: 'assets/images/dog2.png'
+        },
+        {
+            name: 'dog2',
+            img: 'assets/images/dog2.png'
+        },
+        {
+            name: 'dog3',
+            img: 'assets/images/dog3.png'
+        },
+        {
+            name: 'dog3',
+            img: 'assets/images/dog3.png'
+        },
+        {
+            name: 'dog4',
+            img: 'assets/images/dog4.png'
+        },
+        {
+            name: 'dog4',
+            img: 'assets/images/dog4.png'
+        },
+        {
+            name: 'dog5',
+            img: 'assets/images/dog5.png'
+        },
+        {
+            name: 'dog5',
+            img: 'assets/images/dog5.png'
+        },
+        {
+            name: 'dog6',
+            img: 'assets/images/dog6.png'
+        },
+        {
+            name: 'dog6',
+            img: 'assets/images/dog6.png'
+        },
+    ]
+})
